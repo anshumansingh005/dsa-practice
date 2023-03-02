a4a5eb2e3fc2c7c1db40eef00e00c9334119f931
Anshuman Singh <anshumansingh005@gmail.com>
2023-03-02 17:23:49 +0530

    added logic for display

--- birthday-reminder/src/List.js
+++ birthday-reminder/src/List.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
+const List = ({ data }) => {
   return (
     <>
-      <h2>list component</h2>
+      {data.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article className="person" key={id}>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age}</p>
+            </div>
+          </article>
+        );
+      })}
     </>
   );
 };
